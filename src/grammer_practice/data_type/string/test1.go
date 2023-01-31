7e17b122bac04a049457c5abc183d7f6b2558ebc
Siyuan Cheng <siyuan.cheng@shopee.com>
2023-01-31 16:38:35 +0800

    update

--- src/grammer_practice/data_type/string/test1.go
+++ src/grammer_practice/data_type/string/test1.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
 	var s1 = ""
-	us := strings.
+	us := strings.ToUpper(s1)
+	fmt.Println(us)
+
+	var s2 string
+	us2 := strings.ToUpper(s2)
+	fmt.Println(us2)
 }
