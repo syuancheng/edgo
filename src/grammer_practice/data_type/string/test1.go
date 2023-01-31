9a8e3ce5f92eb8a6a1e9775ae5bb3d6b29ca28b7
Siyuan Cheng <siyuan.cheng@shopee.com>
2023-01-31 16:44:02 +0800

    update

--- src/grammer_practice/data_type/string/test1.go
+++ src/grammer_practice/data_type/string/test1.go
@@ -11,6 +11,11 @@ func main() {
 	fmt.Println(us)
 
 	var s2 string
+	fmt.Println(s2 == "")
 	us2 := strings.ToUpper(s2)
 	fmt.Println(us2)
+
+	s3 := "aB"
+	us3 := strings.ToUpper(s3)
+	fmt.Println(us3)
 }
