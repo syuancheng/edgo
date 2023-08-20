d169480bcefebe20e01f6f18f6601714268c88ee
siyuancheng <syuan.cheng21@gmail.com>
2023-08-20 19:29:08 +0800

    update note of func part

--- grammer/function/test4.go
+++ grammer/function/test4.go
@@ -1,16 +1,50 @@
 package main
 
+func testBasic(x int) func() {
+	println(&x)
+
+	return func() {
+		println(&x, x) //返回的匿名函数中直接用到了外部环境的变量
+	}
+}
+
+func test1() []func() {
+	var s []func()
+
+	for i := 0; i < 2; i++ {
+		s = append(s, func() {
+			println(&i, i)
+		})
+	}
+
+	return s
+}
+
+func test2() []func() {
+	var s []func()
+
+	for i := 0; i < 2; i++ {
+		x := i
+		s = append(s, func() {
+			println(&x, x)
+		})
+	}
+
+	return s
+}
+
 func main() {
-	x, y := 1, 2
-	//defer println(x, y)
-	//defer func(a, b int) {
-	//	println(a, b)
-	//}(x, y)
+	//6.闭包
+	f := testBasic(0x100)
+	f()
 
-	defer func() {
-		println(x, y)
-	}()
+	//闭包1
+	for _, f := range test1() {
+		f()
+	}
 
-	x += 100
-	y += 100
+	//闭包2
+	for _, f := range test2() {
+		f()
+	}
 }
