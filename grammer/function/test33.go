b6bebad848bb7f49e794c4d713a01a0f988619d2
Syuan-Cheng <45323956+syuancheng@users.noreply.github.com>
2023-08-21 09:04:46 +0800

    0816 check panic (#17)
    * update note of func part
    
    * update

--- grammer/function/test33.go
+++ grammer/function/test33.go
@@ -24,39 +24,23 @@ func testStruct() {
 	println(x.mul(6, 7))
 }
 
-func testNew(x int) func() {
-	println(&x)
-
-	return func() {
-		println(&x, x)
-	}
-}
-
 func main() {
-	//1. 直接执行
+
 	func(s string) {
 		println(s)
 	}("hell0 syuan!")
 
-	//2.赋值给变量
 	add := func(x, y int) int {
 		return x + y
 	}
 	println(add(1, 5))
 
-	//3. 作为参数，testFn 是一个参数为函数的函数
 	testFn(func() {
 		println("hello testFn")
 	})
 
-	//4. 作为返回值
 	fn := testReturnFn()
 	println(fn(5, 8))
 
-	//5.普通函数和匿名函数都可以作为结构体字段，或经通道传递
 	testStruct()
-
-	//6.闭包
-	f := testNew(0x100)
-	f()
 }
