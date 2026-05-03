2cd43f5dd45a606c1b5c0677d3504bba47b09819
siyuancheng <syuan.cheng21@gmail.com>
2026-05-03 16:38:36 +0800

    ad

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/syuancheng/edgo
 
 go 1.16
 
-require golang.org/x/time v0.15.0
+require (
+	go.uber.org/ratelimit v0.3.1
+	golang.org/x/time v0.15.0
+)
--- src/rate_limit/rate_limit_example.go
+++ src/rate_limit/rate_limit_example.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"time"
+
 	"golang.org/x/time/rate"
 )
 
 func main() {
+	// 每秒生成 2 个令牌,桶容量 5(允许瞬时突发 5 个)
 	limiter := rate.NewLimiter(2, 5)
+
+	ctx := context.Background()
+
+	for i := 1; i <= 10; i++ {
+		// Wait 会阻塞直到拿到一个令牌(或 ctx 取消)
+		if err := limiter.Wait(ctx); err != nil {
+			fmt.Printf("req %d: wait err: %v\n", i, err)
+			continue
+		}
+		fmt.Printf("req %d processed at %s\n", i, time.Now().Format("15:04:05.000"))
+	}
 }
